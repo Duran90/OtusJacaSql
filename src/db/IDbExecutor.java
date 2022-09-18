72224065774901b41aa66450f7955e6dd126019d
Igor Kardanov <Duran.kar90.gmail.com>
2022-09-18 22:43:50 +0300

    Реализован осоновной функционал. Осталоть доработать 2 метода в Query

---
+++ src/db/IDbExecutor.java
@@ -0,0 +1,11 @@
+package db;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+
+public interface IDbExecutor {
+    ResultSet execute (String sqlRequest, boolean isResult) throws SQLException;
+
+    void close() throws SQLException;
+}
